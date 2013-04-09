8d4b6bd45942a6f2643834634b9e75b16c9067dd
Mike Kazantsev <mk.fraggod@gmail.com>
2013-04-09 23:18:32 +0600

    scapy_nflog: untangle netlink-received capture batches in a separate thread

--- scapy_nflog.py
+++ scapy_nflog.py
@@ -1,12 +1,33 @@
-from __future__ import unicode_literals, print_function
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+from threading import Thread
+from collections import deque
+import os
 
 from scapy.all import conf, SuperSocket, ETH_P_ALL, IP, Scapy_Exception
-from nflog_ctypes import nflog_generator
+from nflog_ctypes import nflog_generator, NFWouldBlock
 
 
-class SocketFD(int):
-	'Mock "socket" class to wrap netlink fd int, so that it will work with s.fileno().'
-	def fileno(self): return self
+class NFLOGReaderThread(Thread):
+	'''Necessary only because libnetfilter_log returns
+		packets in batches, and scapy expects to poll/read them one-by-one.'''
+
+	daemon = True
+
+	def __init__(self, queues):
+		super(NFLOGReaderThread, self).__init__()
+		self.queues, self.pipe = queues, deque()
+		self.pipe_chk, self._pipe = os.pipe()
+		self.pipe_chk, self._pipe = os.fdopen(self.pipe_chk), os.fdopen(self._pipe, 'w')
+
+	def run(self):
+		nflog = nflog_generator(self.queues, extra_attrs=['ts'], nlbufsiz=2*2**20)
+		next(nflog)
+		for pkt_info in nflog:
+			self.pipe.append(pkt_info)
+			self._pipe.write('.') # block until other thread reads it
+			self._pipe.flush()
 
 
 class NFLOGListenSocket(SuperSocket):
@@ -19,11 +40,13 @@ class NFLOGListenSocket(SuperSocket):
 			promisc=None, filter=None, nofilter=0, queues=None ):
 		self.type, self.outs = type, None
 		if queues is None: queues = self.queues
-		self.nflog = nflog_generator(queues, extra_attrs=['ts'], nlbufsiz=2*2**20)
-		self.ins = SocketFD(next(self.nflog)) # yields fd first
+		self.nflog = NFLOGReaderThread(queues)
+		self.nflog.start()
+		self.ins = self.nflog.pipe_chk
 
 	def recv(self, bufsize):
-		pkt, ts = next(self.nflog)
+		self.ins.read(1) # used only for poll/sync
+		pkt, ts = self.nflog.pipe.popleft()
 		if pkt is None: return
 		try: pkt = IP(pkt)
 		except KeyboardInterrupt: raise
